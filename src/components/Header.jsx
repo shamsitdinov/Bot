5fed558716191e72de2211ff538fcde49cda51e0
Nodirbek <shamsiddinovnodirbek469@gmail.com>
2025-03-27 19:55:50 +0500

    add

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import bgimg from "../assets/Full-stack-developer_LE_upscale_balanced_x4.jpg";
 const Header = () => {
   return (
     <div>
-      <header className="h-[100vh] w-full rounded-xl" style={{ backgroundImage: `url(${bgimg})`, backgroundRepeat: "no-repeat", backgroundSize:"cover" }}></header>
+      <header className="h-[100vh] w-full rounded-xl max-sm:w-full max-sm:h-[32vh] bg-no-repeat bg-cover" style={{ backgroundImage: `url(${bgimg})`}}></header>
     </div>
   );
 };
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,13 +3,13 @@ import { FaRegStar } from "react-icons/fa";
 
 const Main = ({ deletItem, courses, selectCourse, selectedCourse, sum1 }) => {
   return (
-    <div>
-      <div className="grid bg-[#f6f7f9] py-10 px-10 rounded-md  grid-cols-[1fr,3fr] gap-4 max-md:grid-cols-1 ">
-        <div className="bg-white border-gray-300 border-[1px] text-white max-sm:h-[50vh] rounded-md flex gap-5 flex-col p-4">
-          <h4 className="text-2xl text-green-500 uppercase"> total {sum1}$</h4>
+    <div className="w-[100%] mx-auto">
+      <div className="grid bg-[#f6f7f9 rounded-md max-sm:w-full grid-cols-[1fr,3fr] gap-4 max-md:grid-cols-1 ">
+        <div className="bg-white border-gray-300 border-[1px] max-sm:overflow-auto text-white max-sm:h-[30vh] max-sm:w-full rounded-md flex gap-5 flex-col p-4">
+          <h4 className="text-xl   text-green-500 uppercase max-sm:sticky  bg-white max-sm:-top-5 "> total {sum1}$</h4>
           {selectedCourse
             ? selectedCourse.map((item, index) => (
-                <div key={index} className="flex justify-between shadow-inner shadow-white rounded-md pl-2 bg-slate-800 items-center ">
+                <div key={index} className="flex w-full   justify-between shadow-inner shadow-white rounded-md pl-2 bg-slate-800 items-center ">
                   <h4>{item.title}</h4>
                   <p>{item.price}$</p>
                   <button onClick={() => deletItem(index)} className=" uppercase rounded-md bg-red-500 py-2 px-5">
@@ -21,7 +21,7 @@ const Main = ({ deletItem, courses, selectCourse, selectedCourse, sum1 }) => {
         </div>
         <div className="grid grid-cols-2  gap-4 max-md:grid-cols-1">
           {courses.map((item, index) => (
-            <div key={index} className="flex cursor-pointer bg-white shadow-md shadow-white flex-col gap-4 p-4 rounded-lg border-slate-300 border-[1px] items-center relative min-h-[30vh]">
+            <div key={index} className="flex cursor-pointer bg-white shadow-md shadow-white flex-col gap-4 p-4 rounded-lg border-slate-300 border-[1px] items-center relative min-h-[30vh] max-sm:h-[20vh]">
               <h4 className="text-3xl">{<item.img />}</h4>
               <h3 className="text-3xl">{item.title}</h3>
               <p className="absolute top-2 left-2 bg-red-500 text-white p-2 rounded-md">{item.level}</p>
