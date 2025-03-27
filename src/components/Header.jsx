e1a1255d070b293f942b26458da906c1bc4e79ac
Nodirbek <shamsiddinovnodirbek469@gmail.com>
2025-03-27 18:19:39 +0500

    ss

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import bgimg from "../assets/Full-stack-developer_LE_upscale_balanced_x4.jpg";
 const Header = () => {
   return (
     <div>
-      <header className="h-[100vh] w-full rounded-xl" style={{ backgroundImage: `url(${bgimg})`, backgroundRepeat: "no-repeat", backgroundSize:"coveraa", backgroundPosition: "center" }}></header>
+      <header className="h-[100vh] w-full rounded-xl" style={{ backgroundImage: `url(${bgimg})`, backgroundRepeat: "no-repeat", backgroundSize:"cover", backgroundPosition: "center" }}></header>
     </div>
   );
 };
