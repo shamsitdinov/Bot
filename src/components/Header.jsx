378465401870cd163633cf9e19eebefd799336ed
Nodirbek <shamsiddinovnodirbek469@gmail.com>
2025-03-27 20:53:30 +0500

    feew

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import bgimg from "../assets/11111.jpg";
 const Header = () => {
   return (
-    <div className="h-[100vh]">
+    <div className="h-[100vh] max-sm:h-[32vh]">
       <header className="h-[100%] w-full rounded-xl bg-center max-sm:w-full max-sm:h-[32vh] bg-no-repeat bg-cover" style={{ backgroundImage: `url(${bgimg})` }}></header>
     </div>
   );
